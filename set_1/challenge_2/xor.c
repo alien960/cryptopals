9e5595bd2101556bfc103c96abba2010f392173e
alien960 <trentonwitham@gmail.com>
2022-08-25 04:39:31 +0000

    Updated xor.c with new input method (array as opposed to calloc). Updated challenge 2 notes to new format.

--- set_1/challenge_2/xor.c
+++ set_1/challenge_2/xor.c
@@ -4,73 +4,47 @@
 
 int main(){
 	
-	int inputLen = 256;
-
-	char *input1;
-	char *input2;
-
-	int *hex1;
-	int *hex2;
-
-	int *output;
-	int inputLength;
-
-	input1 = (char*)calloc(inputLen, sizeof(char));
-	input2 = (char*)calloc(inputLen, sizeof(char));
-
-	printf("Input 1:       ");
-	scanf("%s",input1);
-	printf("Input 2:       ");
-	scanf("%s",input2);
-
-	if (strlen(input1) != strlen(input2)){
-		printf("Inputs not of equal length!\n");
-		return 0;
-	}
-
-	inputLength = strlen(input1);
-
-	hex1 = (int*)calloc(inputLength, sizeof(int));
-	hex2 = (int*)calloc(inputLength, sizeof(int));
-
-	output = (int*)calloc(inputLength, sizeof(int));
-
-	for(int i = 0; i < inputLength; i++){
-		if((int)(input1[i]) > 47 && (int)(input1[i]) <  58) hex1[i] = (int)(input1[i]) - 48;
-		else if((int)(input1[i]) > 96 && (int)(input1[i]) < 103) hex1[i] = (int)(input1[i]) - 87;
-		else if((int)(input1[i]) > 64 && (int)(input1[i]) <  71) hex1[i] = (int)(input1[i]) - 55; 	
-		else{
-			printf("Invalid character %c (%d) in Input 1! Hex values (0-9,a-f,A-F) only!\n", input1[i], (int)input1[i]);
-			return 0;
-		}
-	}
+	char feed[256];
+	char key[256];
 	
-	free(input1);
+	int feedIndex = 0;
+	int keyIndex = 0;
 
-	for(int i = 0; i < inputLength; i++){
-		if((int)(input2[i]) > 47 && (int)(input2[i]) <  58) hex2[i] = (int)(input2[i]) - 48;
-		else if((int)(input2[i]) > 96 && (int)(input2[i]) < 103) hex2[i] = (int)(input2[i]) - 87;
-		else if((int)(input2[i]) > 64 && (int)(input2[i]) <  71) hex2[i] = (int)(input2[i]) - 55;	
+	printf("Feed:   ");
+	while((feed[feedIndex] = getchar()) != EOF && feed[feedIndex] != '\n'){
+		feed[feedIndex] = feed[feedIndex] | ' ';
+		if(feed[feedIndex] >= '0' && feed[feedIndex] <= '9') feed[feedIndex] = feed[feedIndex] - '0';
+		else if (feed[feedIndex] >= 'a' && feed[feedIndex] <= 'f') feed[feedIndex] = feed[feedIndex]  - 'W';
 		else{
-			printf("Invalid character %c (%d) in Input 2! Hex values (0-9,a-f,A-F) only!\n", input2[i], (int)input2[i]);
-			return 0;
+			printf("Invalid input! 0-9, a-f, A-F only!\n");
+			return(1);
 		}
+		feedIndex++;
 	}
-	
-	free(input2);
 
-	printf("Xor of inputs: ");
+	printf("Key:    ");
+	while((key[keyIndex] = getchar()) != EOF && key[keyIndex] != '\n'){
+		key[keyIndex] = key[keyIndex] | ' ';
+		if(key[keyIndex] >= '0' && key[keyIndex] <= '9') key[keyIndex] = key[keyIndex] - '0';
+		else if(key[keyIndex] >= 'a' && key[keyIndex] <= 'f') key[keyIndex] = key[keyIndex] - 'W';
+		else{
+			printf("Invalid input! 0-9, a-f, A-F only!\n");
+			return(1);
+		}
+		keyIndex++;
+	}
 
-	for(int i = 0; i < inputLength; i++){
-		output[i] = hex1[i] ^ hex2[i];
-		printf("%x", output[i]);
+	if(feedIndex != keyIndex){
+		printf("Feed and Key must be of equal length!");
+		return(2);
+	}
+
+	printf("Output: ");
+	for(int i = 0; i < feedIndex; i++){
+		printf("%x", feed[i] ^ key[i]);
 	}
 
 	printf("\n");
-
-	free(hex1);
-	free(hex2);
-	free(output);
-
+	
 	return 0;
 }
