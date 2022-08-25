3827044b4fb9548c8fe483e91cc1e8369b9bb7fb
alien960 <trentonwitham@gmail.com>
2022-08-25 03:46:49 +0000

    Cleaned up hex2b64.c for readability, added comments

--- set_1/challenge_1/hex2b64.c
+++ set_1/challenge_1/hex2b64.c
@@ -3,16 +3,18 @@
 
 int main(){
 	
-	char input[256];
-	char hold;
-	char output[256];
+	char input[256];	//For storing input
+	char output[256];	//For storing output
+
+	//Base64 alphabet for reference by algorithm
 	char b64[65] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/','='};
-	int index = 0;
-	int outIndex = 0;
+	
+	int index = 0;          //Index for tracking input quantity and array location
+	int outIndex = 0;       //Index for tracking output quantity and array location
 	
 	printf("Input:  ");
 
-	while((input[index] = getchar()) != EOF && input[index] != '\n'){                //Checks if input is finished and prevents '\n' from being entered in s
+	while((input[index] = getchar()) != EOF && input[index] != '\n'){                //Checks if input is finished and prevents '\n' from being entered in input[]
 		input[index] = input[index] | ' ';                                       //Mask by ORing with [space] =  00100000 to convert uppercase to lowercase
 		if (input[index] >= '0' && input[index] <= '9') input[index] = input[index] - '0';       //Converts '0'-'9' from UTF-8 to digit
 		else if (input[index] >= 'a' && input[index] <= 'f') input[index] = input[index] - 'W';  //Converts 'a'-'f' from UTF-8 to digit
@@ -22,37 +24,42 @@ int main(){
 		}	
 		index++;
 	}
-	if((index * 8) % 6 == 2 || (index * 8) % 6 == 4) input[index] = 0;
-	printf("\n\n%b\n\n", input[index]);
-	for(int i = 0; i < index; i++){
-		if(outIndex % 2 == 0 || outIndex == 0){
-		       	output[outIndex] = b64[(input[i] << 4 | input[i+1]) >> 2];
-			hold = (input[i] << 4 | input[i+1]) >> 2;
-		}
-		else{
-			output[outIndex] = b64[((input[i] & 3) << 4) | input[i+1]];
-			hold = ((input[i] & 3) << 4) | input[i+1];
-			i++;
-		}
-		printf("%c - %b\n", output[outIndex], hold);
-		outIndex++;
-	}
 
-	if((index * 8) % 6 == 2){
-	       	output[outIndex] = '=';
+	//Hex -> b64 algorithm
+	input[index] = 0;		//Add padding for last interpreted character
+	for(int i = 0; i < index; i++){                                              //The pattern of this encoding is composed of two blocks, dubbed even/odd
+		if(outIndex % 2 == 0 || outIndex == 0){                              //Determine if term is even in sequence
+		       	output[outIndex] = b64[(input[i] << 4 | input[i+1]) >> 2];   //Determine b64 char by shifting first term L4 bits and OR with next term  
+		}                                                                    //shifted R2 bits 
+		else{								     //If term isn't even, it's odd
+			output[outIndex] = b64[((input[i] & 3) << 4) | input[i+1]];  //Determine b64 char by shifting first term's 2 LSBs L4 and OR with next term 
+			i++;                                                         //The jigsaw pattern of b64 encoding necessitates this skip as i and outIndex  
+		}                                                                    //get out of sync
+		outIndex++;							     //Probably should've used outIndex as for index as it increments at a constant rate
+	}									     //whereas i increments by 1 or 2
+	
+
+
+	//Determine the amount of '=' needed for the padding used
+	if((index * 8) % 6 == 2){		//'=' if 2 0's padded
+	       	output[outIndex] = '=';        
 		outIndex++;
 	}
-	else if ((index * 8) % 6 == 4){
+	else if ((index * 8) % 6 == 4){         //'==' if 4 0's padded
 		output[outIndex] = '=';
 		output[outIndex] = '=';
 		outIndex = outIndex + 2;
 	}
 
-	printf("Output: " );
 
-	for (int i = 0; i < outIndex; i++){   //Output hex value of each s element
+
+	//Print each element of array output[]
+	printf("Output: " );
+	for (int i = 0; i < outIndex; i++){
 		printf("%c", output[i]);
 	}
 	printf("\n");
+
+
 	return(0);
 }
