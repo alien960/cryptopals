20a178b69bb185fa5b81da2a5d00448820592154
alien960 <trentonwitham@gmail.com>
2022-08-25 03:25:38 +0000

    Challenge 1 completed

--- set_1/challenge_1/hex2b64.c
+++ set_1/challenge_1/hex2b64.c
@@ -1,26 +1,58 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 	
-	long userInput;
+	char input[256];
+	char hold;
+	char output[256];
+	char b64[65] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/','='};
+	int index = 0;
+	int outIndex = 0;
 	
-	int split[64];
-	int counter = 0;
-	
-	scanf("%x", &userInput);
-        printf("%x", userInput);
+	printf("Input:  ");
 
-	while(userInput > 0x0){
-		printf("Entered Loop");
-		split[counter] = userInput % 16;
-		userInput /= 16;
-		printf("%x \n", split[counter]);
-		counter++;
+	while((input[index] = getchar()) != EOF && input[index] != '\n'){                //Checks if input is finished and prevents '\n' from being entered in s
+		input[index] = input[index] | ' ';                                       //Mask by ORing with [space] =  00100000 to convert uppercase to lowercase
+		if (input[index] >= '0' && input[index] <= '9') input[index] = input[index] - '0';       //Converts '0'-'9' from UTF-8 to digit
+		else if (input[index] >= 'a' && input[index] <= 'f') input[index] = input[index] - 'W';  //Converts 'a'-'f' from UTF-8 to digit
+		else {
+			printf("Invalid input! 0-9, a-f, A-F only!\n");                  //If value does not correspond to a hex digit, throw error
+			return(1);
+		}	
+		index++;
 	}
-	
-	//split[0] = userInput % 16;
-	
-	printf("\nTotal: %d \n", counter);
-		
-	return 0;
+	if((index * 8) % 6 == 2 || (index * 8) % 6 == 4) input[index] = 0;
+	printf("\n\n%b\n\n", input[index]);
+	for(int i = 0; i < index; i++){
+		if(outIndex % 2 == 0 || outIndex == 0){
+		       	output[outIndex] = b64[(input[i] << 4 | input[i+1]) >> 2];
+			hold = (input[i] << 4 | input[i+1]) >> 2;
+		}
+		else{
+			output[outIndex] = b64[((input[i] & 3) << 4) | input[i+1]];
+			hold = ((input[i] & 3) << 4) | input[i+1];
+			i++;
+		}
+		printf("%c - %b\n", output[outIndex], hold);
+		outIndex++;
+	}
+
+	if((index * 8) % 6 == 2){
+	       	output[outIndex] = '=';
+		outIndex++;
+	}
+	else if ((index * 8) % 6 == 4){
+		output[outIndex] = '=';
+		output[outIndex] = '=';
+		outIndex = outIndex + 2;
+	}
+
+	printf("Output: " );
+
+	for (int i = 0; i < outIndex; i++){   //Output hex value of each s element
+		printf("%c", output[i]);
+	}
+	printf("\n");
+	return(0);
 }
--- set_1/challenge_2/xor.c
+++ set_1/challenge_2/xor.c
@@ -1,44 +1,76 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(){
 	
-	int inputLen = 36;
-	
-	char input1[inputLen];
-	char input2[inputLen];
+	int inputLen = 256;
 
-	int hex1[inputLen];
-	int hex2[inputLen];
+	char *input1;
+	char *input2;
 
-	int output[inputLen];
-	
-	printf("Input 1: ");
-	scanf("%s", &input1);
-	
-	printf("Input 2: ");
-	scanf("%s", &input2);
+	int *hex1;
+	int *hex2;
 
-	printf("Input 1 Val: %d\n",(int)(input1[0]));
-	printf("Input 2 Val: %d\n",(int)(input2[0]));
+	int *output;
+	int inputLength;
 
-	for(int i = 0; i < inputLen; i++){
-		if((int)(input1[i]) > 47 && (int)(input1[i]) < 58) hex1[i] = (int)(input1[i]) - 48;
-		if((int)(input1[i]) > 96 && (int)(input1[i]) < 103) hex1[i] = (int)(input1[i]) - 87;
-		if((int)(input1[i]) > 64 && (int)(input1[i]) <  71) hex1[i] = (int)(input1[i]) - 55; 	
+	input1 = (char*)calloc(inputLen, sizeof(char));
+	input2 = (char*)calloc(inputLen, sizeof(char));
+
+	printf("Input 1:       ");
+	scanf("%s",input1);
+	printf("Input 2:       ");
+	scanf("%s",input2);
+
+	if (strlen(input1) != strlen(input2)){
+		printf("Inputs not of equal length!\n");
+		return 0;
 	}
-	for(int i = 0; i < inputLen; i++){
-		if((int)(input2[i]) > 47 && (int)(input2[i]) < 58) hex2[i] = (int)(input2[i]) - 48;
-        	if((int)(input2[i]) > 96 && (int)(input2[i]) < 103) hex2[i] = (int)(input2[i]) - 87;
-        	if((int)(input2[i]) > 64 && (int)(input2[i]) <  71) hex2[i] = (int)(input2[i]) - 55;	
+
+	inputLength = strlen(input1);
+
+	hex1 = (int*)calloc(inputLength, sizeof(int));
+	hex2 = (int*)calloc(inputLength, sizeof(int));
+
+	output = (int*)calloc(inputLength, sizeof(int));
+
+	for(int i = 0; i < inputLength; i++){
+		if((int)(input1[i]) > 47 && (int)(input1[i]) <  58) hex1[i] = (int)(input1[i]) - 48;
+		else if((int)(input1[i]) > 96 && (int)(input1[i]) < 103) hex1[i] = (int)(input1[i]) - 87;
+		else if((int)(input1[i]) > 64 && (int)(input1[i]) <  71) hex1[i] = (int)(input1[i]) - 55; 	
+		else{
+			printf("Invalid character %c (%d) in Input 1! Hex values (0-9,a-f,A-F) only!\n", input1[i], (int)input1[i]);
+			return 0;
+		}
 	}
-	for(int i = 0; i < inputLen; i++){
+	
+	free(input1);
+
+	for(int i = 0; i < inputLength; i++){
+		if((int)(input2[i]) > 47 && (int)(input2[i]) <  58) hex2[i] = (int)(input2[i]) - 48;
+		else if((int)(input2[i]) > 96 && (int)(input2[i]) < 103) hex2[i] = (int)(input2[i]) - 87;
+		else if((int)(input2[i]) > 64 && (int)(input2[i]) <  71) hex2[i] = (int)(input2[i]) - 55;	
+		else{
+			printf("Invalid character %c (%d) in Input 2! Hex values (0-9,a-f,A-F) only!\n", input2[i], (int)input2[i]);
+			return 0;
+		}
+	}
+	
+	free(input2);
+
+	printf("Xor of inputs: ");
+
+	for(int i = 0; i < inputLength; i++){
 		output[i] = hex1[i] ^ hex2[i];
 		printf("%x", output[i]);
 	}
 
-
 	printf("\n");
+
+	free(hex1);
+	free(hex2);
+	free(output);
 
 	return 0;
 }
