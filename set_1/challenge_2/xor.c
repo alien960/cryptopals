dcfd977e68ee4256b4aca847ef11894922eee093
alien960 <trentonwitham@gmail.com>
2022-08-27 03:32:05 +0000

    Implemented changes from challenge 1

--- set_1/challenge_2/xor.c
+++ set_1/challenge_2/xor.c
@@ -11,6 +11,8 @@ int main(){
 	int keyIndex = 0;
 
 	printf("Feed:   ");
+
+/*
 	while((feed[feedIndex] = getchar()) != EOF && feed[feedIndex] != '\n'){
 		feed[feedIndex] = feed[feedIndex] | ' ';
 		if(feed[feedIndex] >= '0' && feed[feedIndex] <= '9') feed[feedIndex] = feed[feedIndex] - '0';
@@ -21,8 +23,24 @@ int main(){
 		}
 		feedIndex++;
 	}
+*/
+
+	for(feedIndex; (feed[feedIndex] = getchar()) != EOF; feedIndex++){
+		feed[feedIndex] = feed[feedIndex] | ' ';
+		if((feed[feedIndex] >= '0') && (feed[feedIndex] <= '9')) feed[feedIndex] = feed[feedIndex] - '0';
+		else if ((feed[feedIndex] >= 'a') && (feed[feedIndex] <= 'f')) feed[feedIndex] = feed[feedIndex] - 'W';
+		else if (feed[feedIndex] == '*'){
+		       //	feedIndex--;
+			break;
+		}
+		else{
+			printf("Invalid input! 0-9, a-f, A-F only!\n");
+			return(1);
+		}
+	}
 
 	printf("Key:    ");
+	/*
 	while((key[keyIndex] = getchar()) != EOF && key[keyIndex] != '\n'){
 		key[keyIndex] = key[keyIndex] | ' ';
 		if(key[keyIndex] >= '0' && key[keyIndex] <= '9') key[keyIndex] = key[keyIndex] - '0';
@@ -32,6 +50,21 @@ int main(){
 			return(1);
 		}
 		keyIndex++;
+	}
+	*/
+
+	for(keyIndex; (key[keyIndex] = getchar()) != EOF; keyIndex++){
+		key[keyIndex] = key[keyIndex] | ' ';
+		if((key[keyIndex] >= '0' && key[keyIndex] <= '9')) key[keyIndex] = key[keyIndex] - '0';
+		else if((key[keyIndex] >= 'a') && (key[keyIndex] <= 'f')) key[keyIndex] = key[keyIndex] - 'W';
+		else if(key[keyIndex] == '*'){
+			//keyIndex--;       
+			break;
+		}
+		else{
+			printf("Invalid input! 0-9, a-f, A-F only!\n");
+			return(1);
+		}
 	}
 
 	if(feedIndex != keyIndex){
