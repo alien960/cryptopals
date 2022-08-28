d23b4f15241ef70f20ebc5fea8784ca0060ef737
alien960 <trentonwitham@gmail.com>
2022-08-28 19:14:44 +0000

    Completed challenge, code needs cleanup and rework in some areas

--- set_1/challenge_5/repeatingKey.c
+++ set_1/challenge_5/repeatingKey.c
@@ -12,24 +12,41 @@ int main(){
 	int keyIndex = 0;
 
 	printf("Plaintext Message:   ");
-	while((plaintext[plaintextIndex] = getchar()) != EOF && plaintext[plaintextIndex] != '\n') plaintextIndex++;
+	while((plaintext[plaintextIndex] = getchar()) != EOF && plaintext[plaintextIndex] != '\n') plaintextIndex++;	
+		
 
-	printf("\n\nKey:                 ");
+
+
+	printf("Key:                 ");
 	while((key[keyIndex] = getchar()) != EOF && key[keyIndex] != '\n') keyIndex++;
-	
+
 	for(int i = 0; i < plaintextIndex; i++){
 		if(i == 0){
 			encrypted[i] = plaintext[i] ^ key[i];
 		}
 		else{
-			encrypted[i] = plaintext[i] ^ key[keyIndex - (i % keyIndex)];	
+			encrypted[i] = plaintext[i] ^ key[i % keyIndex];
 		}
 	}	
 
-	printf("\n\nCiphertext Message:   ");
+	printf("\n\nCiphertext Message:   "); 
+	
 	for(int i = 0; i < plaintextIndex; i++){
-		printf("%c%", encrypted[i]);
+		//if((encrypted[i] & 16) <= 9){
+		   	printf("%x", (encrypted[i] >> 4));
+		//}
+		//else if((encrypted[i] & 16) <= 16){
+		//	printf("%c", (encrypted[i] & 16) + 'W');
+		//}
+		//if((encrypted[i] >> 4) <= 9) {
+			printf("%x", (encrypted[i] & 15));
+		//}
+		//else{
+		//	printf("%c", (encrypted[i] >> 4) + 'W');
+		//}
 	}
-
+	
+	printf("\n");
 	return 0;
+
 }
